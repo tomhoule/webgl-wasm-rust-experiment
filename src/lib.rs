d8d59865fbd1c9dbf2b62a16d4ac09c972f4e709
Tom Houlé <tom@tomhoule.com>
2017-11-28 20:56:52 +0100

    First experiment - a shrinking square

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+mod positions;
+
+#[no_mangle]
+pub fn do_the_thing(n: i32) -> i32 {
+    9001 + n
+}
+
+#[no_mangle]
+pub fn initial_positions() -> *mut f32 {
+    let mut vec: Vec<f32> = vec![
+         1.0,  1.0,
+        -1.0,  1.0,
+         1.0, -1.0,
+        -1.0, -1.0,
+    ];
+    vec.as_mut_ptr()
+}
+
+#[no_mangle]
+pub fn update_positions(vertices: *mut f32) { //-> *mut f32 {
+    use std::slice;
+    unsafe {
+        let slice: &mut [f32] = slice::from_raw_parts_mut(vertices, 8);
+        let sum: f32 = slice.iter().sum();
+        for vertex in slice.iter_mut() {
+            if sum > 2.0 { // expanding
+                *vertex *= 1.01
+            } else { // collapsing
+                *vertex *= 0.99
+            }
+
+        }
+    }
+}
